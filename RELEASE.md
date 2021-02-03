987b52e58ea2e5bcf2eade97bfcc31ef72c316ca
Thomas Duigou <thomas.duigou@inrae.fr>
2021-02-03 17:18:40 +0100

    build: ready for conda package

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,8 @@
 # Release history
 
+## 0.0.2
+- Packaged version 
+
 ## 0.0.1
 - Hello, world
 
--- setup.py
+++ setup.py
@@ -1,16 +1,47 @@
 from setuptools import setup, find_packages
+import os
+import re
+
+_readme = 'README.md'
+_extras = 'extras'
+
+with open(_readme, 'r', encoding='utf-8') as f:
+    _long_description = f.read()
+
+with open(os.path.join(_extras, '.env'), 'r', encoding='utf-8') as f:
+    for line in f:
+        if line.startswith('PACKAGE='):
+            _package = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('URL='):
+            _url = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('AUTHORS='):
+            _authors = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('DESCR='):
+            _descr = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('CORR_AUTHOR='):
+            _corr_author = line.splitlines()[0].split('=')[1].lower()
+
+with open(os.path.join(_package, '_version.py'), 'r') as f:
+    m = re.search('"(.+)"', f.readline().split('=')[1])
+    if m:
+        _version = m.group(1)
 
 setup(
-    name='rpbasicdesign',
-    version='0.0.1',
-    description='Convert rpSBML enzyme info in to BASIC construct.',
-    license='MIT',
-    author='Thomas Duigou',
-    author_email='thomas.duigou@inrae.fr',
-    url='https://github.com/brsynth/dnaprep',
+    name=_package,
+    version=_version,
+    author=_authors,
+    author_email=_corr_author,
+    description=_descr,
+    long_description=_long_description,
+    long_description_content_type='text/markdown',
+    url=_url,
     packages=find_packages(),
-    keywords=['rpbasicdesign'],
+    include_package_data=True,
+    test_suite='pytest',
+    license='MIT',
     classifiers=[
         'Topic :: Scientific/Engineering',
-    ]
+    ],
+    keywords=[_package],
+    # python_requires='>=3.6',
 )
