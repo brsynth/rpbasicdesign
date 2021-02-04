cc671da1ae9676325507edd5fa085d5ce26b5f0b
Thomas Duigou <thomas.duigou@inrae.fr>
2021-02-04 18:08:33 +0100

    refactor: add constants

---
+++ rpbasicdesign/constants.py
@@ -0,0 +1,8 @@
+DNABOT_PART_HEADER = ['Part/linker', 'Well', 'Part concentration (ng/uL)']
+DNABOT_CONSTRUCT_HEADER = ['Well',
+                           'Linker 1', 'Part 1', 'Linker 2', 'Part 2',
+                           'Linker 3', 'Part 3', 'Linker 4', 'Part 4',
+                           'Linker 5', 'Part 5', 'Linker 6', 'Part 6',
+                           'Linker 7', 'Part 7', 'Linker 8', 'Part 8',
+                           'Linker 9', 'Part 9', 'Linker 10', 'Part 10',
+                            ]
