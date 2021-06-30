0fd0473b42f0c9e5ad62c013804c311d74c57507
Thomas Duigou <thomas.duigou@inrae.fr>
2021-06-30 14:47:15 +0200

    style(cli.py): update

--- rpbasicdesign/cli.py
+++ rpbasicdesign/cli.py
@@ -17,48 +17,88 @@ from rpbasicdesign.Designer import Designer
 def __cli():
     """CLI for dnaprep."""
 
-    desc = "Convert rpSBML enzyme info in to BASIC construct. UniProt IDs corresponding enzyme variants are extracted" \
-           " from rpSBMl files. Promoters and RBSs are randomly chosen from a default list. CDSs, in other" \
-           " words gene variants, of enzymes are randomly chosen from amongst the UniProt IDs extracted. Constructs" \
-           " generated can be stored as (i) a CSV file ready to be used by DNA-Bot, (ii) as SBOL files. "
-    parser = argparse.ArgumentParser(description=desc, prog='python -m dnaprep.cli')
-    parser.add_argument('--rpsbml_file',
-                        help='rpSBML file from which enzymes UniProt IDs will be collected',
-                        required=True)
-    parser.add_argument('--parts_files',
-                        help='List of files providing available linkers and user parts (backbone, promoters, ...) for constructs.',
-                        type=str, nargs='+')
-    parser.add_argument('--monocistronic',
-                        help='Build monocistronic constructs. Default to false, ie polycistronic constructs will be '
-                             'generated.',
-                        default=False, type=lambda x: (str(x).lower() == 'true'))
-    parser.add_argument('--lms_id', help='part ID to be used as the LMS methylated linker', default='LMS')
-    parser.add_argument('--lmp_id', help='part ID to be used as the LMP methylated linker', default='LMP')
-    parser.add_argument('--backbone_id', help='part ID to be used as the backbone', default='BASIC_SEVA_37_CmR-p15A.1')
-    parser.add_argument('--sample_size', help='Number of construct to generate.', default=3, type=int)
-    parser.add_argument('--o_dnabot_dir',
-                        help='Output folder to write construct and coord part files. It will be created if it does '
-                             'not exist yet. Existing files will be overwritten. Default: out/dnabot_in',
-                        default='out/dnabot_in')
-    parser.add_argument('--o_sbol_dir',
-                        help='Output folder to write SBOL depictions of constructs. It will be created if it does not '
-                             'exist yet. Existing files will be overwritten. Default: out/sbol_export',
-                        default='out/sbol_export')
+    desc = "Convert rpSBML enzyme info in to BASIC construct. UniProt IDs" \
+           " corresponding enzyme variants are extracted rpSBMl files." \
+           " Promoters and RBSs are randomly chosen from a default list." \
+           " CDSs, in other words gene variants, of enzymes are randomly" \
+           " chosen from amongst the UniProt IDs extracted. Constructs" \
+           " generated can be stored as (i) a CSV file ready to be used by"\
+           " DNA-Bot, (ii) as SBOL files."
+    parser = argparse.ArgumentParser(
+         description=desc,
+         prog='python -m dnaprep.cli'
+         )
+    parser.add_argument(
+         '--rpsbml_file',
+          help='rpSBML file from which enzymes UniProt IDs will be collected',
+          required=True
+          )
+    parser.add_argument(
+         '--parts_files',
+          help='List of files providing available linkers and user parts '
+               '(backbone, promoters, ...) for constructs.',
+          type=str,
+          nargs='+'
+          )
+    parser.add_argument(
+         '--monocistronic',
+          help='Build monocistronic constructs. Default to false, ie '
+               'polycistronic constructs will be generated.',
+          default=False,
+          type=lambda x: (str(x).lower() == 'true')
+          )
+    parser.add_argument(
+         '--lms_id',
+         help='part ID to be used as the LMS methylated linker',
+         default='LMS'
+         )
+    parser.add_argument(
+         '--lmp_id',
+         help='part ID to be used as the LMP methylated linker',
+         default='LMP'
+         )
+    parser.add_argument(
+         '--backbone_id',
+         help='part ID to be used as the backbone',
+         default='BASIC_SEVA_37_CmR-p15A.1'
+         )
+    parser.add_argument(
+         '--sample_size',
+         help='Number of construct to generate.',
+         default=3,
+         type=int
+         )
+    parser.add_argument(
+         '--o_dnabot_dir',
+          help='Output folder to write construct and coord part files. '
+               'It will be created if it does not exist yet. Existing '
+               'files will be overwritten. Default: out/dnabot_in',
+          default='out/dnabot_in'
+          )
+    parser.add_argument(
+         '--o_sbol_dir',
+          help='Output folder to write SBOL depictions of constructs. '
+               'It will be created if it does not exist yet. Existing '
+               'files will be overwritten. Default: out/sbol_export',
+          default='out/sbol_export'
+          )
 
     # Logging
     logging.basicConfig(
-            stream=sys.stderr, level=logging.INFO,
-            datefmt='%d/%m/%Y %H:%M:%S',
-            format='%(asctime)s -- %(levelname)s -- %(message)s'
-            )
+          stream=sys.stderr, level=logging.INFO,
+          datefmt='%d/%m/%Y %H:%M:%S',
+          format='%(asctime)s -- %(levelname)s -- %(message)s'
+          )
 
     # Compute
     args = parser.parse_args()
-    o = Designer(monocistronic=args.monocistronic,
-                 lms_id=args.lms_id, lmp_id=args.lmp_id,
-                 backbone_id=args.backbone_id,
-                 parts_files=args.parts_files
-                 )
+    o = Designer(
+         monocistronic=args.monocistronic,
+          lms_id=args.lms_id,
+          lmp_id=args.lmp_id,
+          backbone_id=args.backbone_id,
+          parts_files=args.parts_files
+          )
     o.enzyme_from_rpsbml(rpsbml_file=args.rpsbml_file)
     nb_constructs = o.combine(sample_size=args.sample_size)
     logging.info(f'{nb_constructs} generated.')
