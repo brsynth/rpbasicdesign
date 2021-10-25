5c07addc9c14f08c617b139793b45dd619e0d3ce
Thomas Duigou <thomas.duigou@inrae.fr>
2021-10-25 18:46:03 +0200

    tests(test_Designer): update assertions

--- tests/test_Designer.py
+++ tests/test_Designer.py
@@ -31,29 +31,28 @@ def test_init_2():
 def test_extract_from_rpsbml():
     o = Designer(lms_id='LMS', lmp_id='LMP',
                  backbone_id='BASIC_SEVA_37_CmR-p15A.1')
-    o.enzyme_from_rpsbml(rpsbml_file=_RPSBML_PATH)
+    o.get_selenzyme_annotation(rpsbml_path=_RPSBML_PATH)
 
 
 def test_combine():
     o = Designer(lms_id='LMS', lmp_id='LMP',
                  backbone_id='BASIC_SEVA_37_CmR-p15A.1')
-    o.enzyme_from_rpsbml(rpsbml_file=_RPSBML_PATH)
+    o.get_selenzyme_annotation(rpsbml_path=_RPSBML_PATH)
     assert o.combine(sample_size=0, random_seed=42) == 0
     assert o.combine(sample_size=12, random_seed=42, cds_permutation=False) == 12
     assert isinstance(o.constructs, list)
     print(o.constructs[0].get_part_ids())
     assert o.constructs[0].get_part_ids() == [
         'LMS', 'BASIC_SEVA_37_CmR-p15A.1',
-        'LMP', 'PJ23104_BASIC',
-        'U1-RBS1', 'Q8XEC3',
-        'U2-RBS1', 'P23262',
-        'U3-RBS2', 'P95607']
+        'LMP', 'PJ23119_BASIC',
+        'U1-RBS2', 'O85675',
+        'U2-RBS2', 'O33948']
 
 
 def test_write_dnabot_inputs(tmp_path):
     o = Designer(lms_id='LMS', lmp_id='LMP',
                  backbone_id='BASIC_SEVA_37_CmR-p15A.1')
-    o.enzyme_from_rpsbml(rpsbml_file=_RPSBML_PATH)
+    o.get_selenzyme_annotation(rpsbml_path=_RPSBML_PATH)
     o.combine(sample_size=10, random_seed=42)
     o.write_dnabot_inputs(out_dir=tmp_path)
     files = list(tmp_path.iterdir())
@@ -64,7 +63,7 @@ def test_write_dnabot_inputs(tmp_path):
 def test_write_sbol(tmp_path):
     o = Designer(lms_id='LMS', lmp_id='LMP',
                  backbone_id='BASIC_SEVA_37_CmR-p15A.1')
-    o.enzyme_from_rpsbml(rpsbml_file=_RPSBML_PATH)
+    o.get_selenzyme_annotation(rpsbml_path=_RPSBML_PATH)
     o.combine(sample_size=10, random_seed=42)
     o.write_sbol(out_dir=tmp_path)
     files = list(tmp_path.iterdir())
